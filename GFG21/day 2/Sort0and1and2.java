a5e69a633238366d196027db91650a8724b9ada7
bayyaramharsha <bayyaramharsha@gmail.com>
2023-06-13 02:32:20 +0530

    day2-problem1

---
+++ GFG21/day 2/Sort0and1and2.java
@@ -0,0 +1,65 @@
+//https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+
+// Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+// Example 1:
+
+// Input: 
+// N = 5
+// arr[]= {0 2 1 2 0}
+// Output:
+// 0 0 1 2 2
+// Explanation:
+// 0s 1s and 2s are segregated 
+// into ascending order.
+// Example 2:
+
+// Input: 
+// N = 3
+// arr[] = {0 1 0}
+// Output:
+// 0 0 1
+// Explanation:
+// 0s 1s and 2s are segregated 
+// into ascending order.
+
+// Your Task:
+// You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
+
+
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(1)
+
+
+// Constraints:
+// 1 <= N <= 10^6
+// 0 <= A[i] <= 2
+
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+            if(a[mid]==0){
+                int temp = a[mid];//swapping
+                a[mid] = a[low];
+                a[low] = temp;
+                low++;
+                mid++;
+            }
+            else if(a[mid]==1){
+                mid++;
+            }
+            else{
+                int temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+                high--;
+            }
+        }
+    }
+}
