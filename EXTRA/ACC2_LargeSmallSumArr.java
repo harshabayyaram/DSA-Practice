c503bf3d705f4ba6600e0ec79cdb2afe7fc82f13
Harshath kumar Bayyaram <bayyaramharsha@gmail.com>
2023-07-30 01:38:06 +0530

    Update ACC2_LargeSmallSumArr.java

--- EXTRA/ACC2_LargeSmallSumArr.java
+++ EXTRA/ACC2_LargeSmallSumArr.java
@@ -1,5 +1,7 @@
 /*
 
+https://www.codingninjas.com/studio/library/30-most-common-accenture-coding-questions
+
 def LargeSmallSum(arr)
 
 The function takes an integral arr which is of the size or length of its arguments. Return the sum of the second smallest element at odd position ‘arr’ and the second largest element at the even position.
