16703e64d18ade1a52c7a4822799ae02a92ba62a
Harshath kumar Bayyaram <bayyaramharsha@gmail.com>
2023-07-31 22:31:23 +0530

    Solved Print 1 to N without loop.java

---
+++ GFG/PRint 1 to N without loop.java
@@ -0,0 +1,52 @@
+/*
+
+https://practice.geeksforgeeks.org/problems/print-1-to-n-without-using-loops-1587115620/1?page=1&difficulty[]=-2&sortBy=submissions
+
+
+Print numbers from 1 to N without the help of loops.
+
+Example 1:
+
+Input:
+N = 10
+Output: 1 2 3 4 5 6 7 8 9 10
+
+Example 2:
+
+Input:
+N = 5
+Output: 1 2 3 4 5
+ 
+
+Your Task:
+This is a function problem. You only need to complete the function printNos() that takes N as parameter and prints number from 1 to N recursively. Don't print newline, it will be added by the driver code.
+
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N) (Recursive).
+
+
+Constraints:
+1 <= N <= 1000
+
+
+*/
+
+
+
+class Solution
+{
+    
+  public void printNos(int N)
+    {
+        //Your code here
+        if(N<=0){
+            return;
+        }
+        else{
+            printNos(N-1);
+            System.out.print(N+" ");
+        }
+    }
+}
+
